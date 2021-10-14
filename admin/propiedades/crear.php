570a8674f3656bfb187e103979eccf2219abeecf
Dani <nikiforovdaniel@gmail.com>
2021-10-14 19:03:32 -0300

    Formulario para agregar propiedades

---
+++ admin/propiedades/crear.php
@@ -0,0 +1,60 @@
+<?php 
+    require '../../includes/funciones.php';
+    
+    incluirTemplate('header');
+    
+
+?>
+    <main class="contenedor seccion">
+        <h1>Crear</h1>
+
+        <a href="/admin" class="boton-verde">Volver</a>
+
+        <form class="form">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo de la Propiedad">
+                
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio de la Propiedad">
+                
+                <label for="imagen">Imagenes:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Información de la Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+               
+                <label for="wc">Habitaciones:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+                
+                <label for="estacionamiento">Habitaciones:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="99">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor/es</legend>
+
+                <select>
+                    <option value="1">Daniel</option>
+                    <option value="2">Jorge</option>
+                    <option value="3">Carla</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton-verde">
+        </form>
+
+    </main>
+
+    <?php incluirTemplate('footer'); ?>
+
