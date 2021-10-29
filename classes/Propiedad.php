0487ca7e67196e3688eb8a27500465d757f9a242
Dani <nikiforovdaniel@gmail.com>
2021-10-29 20:15:19 -0300

    Mapeo de atributos y sanitización

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -23,7 +23,12 @@ class Propiedad {
 class Propiedad {
     // BASE DE DATOS
     protected static $db;
+    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'estacionamiento', 'wc', 'creado', 'vendedor_ID'];
 
+    // Definir conexión a BD
+    public static function setDB($database) {
+        self::$db = $database;
+    }
     public $id;
     public $titulo;
     public $precio;
@@ -36,19 +41,23 @@ class Propiedad {
     public $vendedor_ID;
 
     public function __construct($args = []) {
-    $this->id = $args['id'] ?? '';
-    $this->titulo = $args['titulo'] ?? '';
-    $this->precio = $args['precio'] ?? '';
-    $this->imagen = $args['imagen'] ?? '';
-    $this->descripcion = $args['descripcion'] ?? '';
-    $this->habitaciones = $args['habitaciones'] ?? '';
-    $this->estacionamiento = $args['estacionamiento'] ?? '';
-    $this->wc = $args['wc'] ?? '';
-    $this->creado = date('Y/m/d');
-    $this->vendedor_ID = $args['vendedor_ID'] ?? '';
+        $this->id = $args['id'] ?? '';
+        $this->titulo = $args['titulo'] ?? '';
+        $this->precio = $args['precio'] ?? '';
+        $this->imagen = $args['imagen'] ?? '';
+        $this->descripcion = $args['descripcion'] ?? '';
+        $this->habitaciones = $args['habitaciones'] ?? '';
+        $this->estacionamiento = $args['estacionamiento'] ?? '';
+        $this->wc = $args['wc'] ?? '';
+        $this->creado = date('Y/m/d');
+        $this->vendedor_ID = $args['vendedor_ID'] ?? '';
     }
+    
 
     public function guardar() {
+        // Sanitización
+        $atributos = $this->sanitizarAtributos();
+
         $query = "INSERT INTO propiedades ( 
             titulo, 
             precio, 
@@ -74,12 +83,33 @@ class Propiedad {
             )";
 
             $resultado = self::$db->query($query);
-
-        }   
+        
  
-    // Definir conexión a BD
-    public static function setDB($database) {
-        self::$db = $database;
+    }
+
+
+    public function atributos() {
+        $atributos = [];
+        foreach(self::$columnasDB as $columna) {
+            if($columna === '$id') continue;
+            $atributos[$columna] = $this->$columna;
+        }
+        debug($atributos);
+        return $atributos;
+    }
+
+
+    public function sanitizarAtributos() {
+        
+        $atributos = $this->atributos();
+        $sanitizado = [];
+        
+        foreach($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+
+        return $sanitizado;
+
     }
 
 }
