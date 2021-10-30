8af5876e8bc2c9912b1ddd90276475064e25dc12
Dani <nikiforovdaniel@gmail.com>
2021-10-29 21:00:52 -0300

    Guardando en DB P00

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -1,24 +1,7 @@
 <?php
 
 namespace App;
-/*
-class Propiedad {
-    public function __construct(        
-        public $id,
-        public $titulo,
-        public $precio,
-        public $imagen,
-        public $descripcion,
-        public $habitaciones,
-        public $wc,
-        public $creado,
-        public $vendedor_ID) {
 
-    }
- 
-
-}^
-*/
 
 class Propiedad {
     // BASE DE DATOS
@@ -44,7 +27,7 @@ class Propiedad {
         $this->id = $args['id'] ?? '';
         $this->titulo = $args['titulo'] ?? '';
         $this->precio = $args['precio'] ?? '';
-        $this->imagen = $args['imagen'] ?? '';
+        $this->imagen = $args['imagen'] ?? 'imagen.jpg';
         $this->descripcion = $args['descripcion'] ?? '';
         $this->habitaciones = $args['habitaciones'] ?? '';
         $this->estacionamiento = $args['estacionamiento'] ?? '';
@@ -52,13 +35,85 @@ class Propiedad {
         $this->creado = date('Y/m/d');
         $this->vendedor_ID = $args['vendedor_ID'] ?? '';
     }
-    
+
 
     public function guardar() {
         // Sanitización
         $atributos = $this->sanitizarAtributos();
 
-        $query = "INSERT INTO propiedades ( 
+
+        // Inserción en BD
+        $query = "INSERT INTO propiedades ( ";
+        $query .= join(', ', array_keys($atributos));
+        $query .= " ) VALUES (' ";
+        $query .= join("', '", array_values($atributos));
+        $query .= " ') ";
+
+        $resultado = self::$db->query($query);
+
+    }
+
+
+    public function atributos() {
+        $atributos = [];
+        foreach(self::$columnasDB as $columna) {
+            if($columna === '$id') continue;
+            $atributos[$columna] = $this->$columna;
+        }
+        return $atributos;
+    }
+
+
+    public function sanitizarAtributos() {
+        
+        $atributos = $this->atributos();
+        $sanitizado = [];
+        
+        foreach($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+
+        return $sanitizado;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Propiedad {
+    public function __construct(        
+        public $id,
+        public $titulo,
+        public $precio,
+        public $imagen,
+        public $descripcion,
+        public $habitaciones,
+        public $wc,
+        public $creado,
+        public $vendedor_ID) {
+
+    }
+ 
+
+}^
+*/
+
+
+
+
+/*         
+QUERY ORIGINAL
+    $query = "INSERT INTO propiedades ( 
             titulo, 
             precio, 
             imagen,
@@ -82,34 +137,4 @@ class Propiedad {
                 '$this->vendedor_ID'
             )";
 
-            $resultado = self::$db->query($query);
-        
- 
-    }
-
-
-    public function atributos() {
-        $atributos = [];
-        foreach(self::$columnasDB as $columna) {
-            if($columna === '$id') continue;
-            $atributos[$columna] = $this->$columna;
-        }
-        debug($atributos);
-        return $atributos;
-    }
-
-
-    public function sanitizarAtributos() {
-        
-        $atributos = $this->atributos();
-        $sanitizado = [];
-        
-        foreach($atributos as $key => $value) {
-            $sanitizado[$key] = self::$db->escape_string($value);
-        }
-
-        return $sanitizado;
-
-    }
-
-}
+            */
