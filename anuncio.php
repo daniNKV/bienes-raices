6f1fcb831bbf2e558b17117039a8e7dce4e5df01
Daniel <83901326+daniNKV@users.noreply.github.com>
2021-11-25 04:12:49 -0300

    Mostrando propiedades con POO

--- anuncio.php
+++ anuncio.php
@@ -1,7 +1,7 @@
 <?php 
-    // Importar conexión
     require 'includes/app.php';
-    $db = conectarDB();
+    use App\Propiedad;
+
 
     $id = $_GET['id'];
     $id = filter_var($id, FILTER_VALIDATE_INT );
@@ -9,47 +9,41 @@
     if(!$id) {
         header('Location: /');
     }
-   
-    // Consulta
-    $query = "SELECT * FROM propiedades WHERE id = ${id}";
 
-    // Resultado
-    $resultado = mysqli_query($db, $query);
-    $propiedad = mysqli_fetch_assoc($resultado);
+    $propiedad = Propiedad::find($id);
 
-
-        incluirTemplate('header');
+    incluirTemplate('header');
     
 ?>
     
 <main class="contenedor seccion contenido-centrado">
 
-    <h1><?php echo $propiedad['titulo']?></h1>
+    <h1><?php echo $propiedad->titulo; ?> </h1>
 
     <picture>
-        <img src="imagenes/<?php echo $propiedad['imagen']?>" alt="Propiedad" loading="lazy">
+        <img src="imagenes/<?php echo $propiedad->imagen?>" alt="Propiedad" loading="lazy">
     </picture>
 
     <div class="resumen-propiedad">
-        <p class="precio">Precio: $<?php echo $propiedad['precio']?></p>
+        <p class="precio">Precio: $<?php echo $propiedad->precio; ?> </p>
 
         <ul class="iconos-caracteristicas">
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                <p><?php echo $propiedad['wc']?></p>
+                <p><?php echo $propiedad->wc; ?></p>
             </li>
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono icono estacionamiento">
-                <p><?php echo $propiedad['estacionamiento']?></p>
+                <p><?php echo $propiedad->estacionamiento;?></p>
             </li>
             <li>
                 <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                <p><?php echo $propiedad['habitaciones']?></p>
+                <p><?php echo $propiedad->habitaciones;?></p>
             </li>
         </ul>
 
         <p>
-            <?php echo $propiedad['descripcion']?>
+            <?php echo $propiedad->descripcion;?>
         </p>
 
         
